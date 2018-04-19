abd00c797ddfba9e7aa181c668a59f27053355fb
smile-xy <904128532@qq.com>
2018-04-19 22:10:40 +0800

    准备复习几天

--- myTool.js
+++ myTool.js
@@ -52,20 +52,94 @@ var inherit = (function () {
     }
 }());
 
-// 获取当前窗口大小，并返回数组[pageWidth, pageHeight];
+// 获取当前窗口大小，并返回对象{pageWidth, pageHeight};
 var getPageSize = function () {
-    var pageWidth = window.innerWidth,
-        pageHeight = window.innerHeight;
-
-    if (typeof pageWidth != 'number') {
+    if (window.innerWidth) {
+        return {
+            pageWidth: window.innerWidth,
+            pageHeight: window.innerHeight
+        }
+    } else {
         if (document.compatMode == 'CSS1Compat') {
-
-            pageWidth = document.documentElement.clientWidth;
-            pageHeight = document.documentElement.clientHeight;
+            return {
+                pageWidth: document.documentElement.clientWidth,
+                pageHeight: document.documentElement.clientHeight
+            }
         } else {
-            pageWidth = document.body.clientWidth;
-            pageHeight = document.body.clientHeight;
+            return {
+                pageWidth: document.body.clientWidth,
+                pageHeight: document.body.clientHeight
+            }
         }
     }
-    return [pageWidth, pageHeight];
 };
+
+// 获取当前滚动条滚动距离，并返回对象{xOffset, yOffset};
+function getScrollOffset(){
+        
+    if (window.pageXOffset) {
+        return {
+            xOffset: window.pageXOffset,
+            yOffset: window.pageYOffset
+        }
+    }else {
+        return {
+            xOffset: document.body.scrollLeft + document.documentElement.scrollLeft,
+            yOffset: document.body.scrollTop + document.documentElement.scrollTop
+        }
+    }
+};
+
+// 获取 elem元素的 prop属性的最终显示结果并返回
+function getElementStyle(elem, prop) {
+    if (window.getComputedStyle) {
+        return window.getComputedStyle(elem, null)[prop];
+    } else {
+        return elem.currentStyle[prop];
+    }
+};
+
+// 跨浏览器 事件处理方法
+var EventTool = {
+    addHandler: function (elem, type, handler) {
+        if (elem.addEventListener) {
+            elem.addEventListener(type, handler, false);
+        } else if (elem.attachEvent) {
+            elem.attachEvent('on' + type, handler)
+        } else {
+            elem['on' + type] = handler;
+        }
+    },
+    removeHandler: function (elem, type, handler) {
+        if (elem.removeEventListener) {
+            elem.removeEventListener(type, handler, false);
+        } else if (elem.detachEvent) {
+            elem.detachEvent('on' + type, handler)
+        } else {
+            elem['on' + type] = null;
+        }
+    },
+}
+
+// 跨浏览器 事件绑定 (解决了IE this指向 window 的BUG)
+function addEvent(elem, type, handler) {
+    if (elem.addEventListener) {
+        elem.addEventListener(type, handler, false);
+    } else if(elem.attachEvent){
+        elem.attachEvent('on' + type, function(){
+            handler.call(elem);
+        })
+    }else {
+        elem['on' + type] = handler;
+    }
+}
+
+// 跨浏览器 取消默认事件
+function cancelHandler(event) {
+    var event = event || window.event;
+    if (event.preventDefault) {
+        event.preventDefault();
+    } else {
+        event.returnValue = false;
+    }
+}
